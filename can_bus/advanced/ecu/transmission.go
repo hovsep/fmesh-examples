a20e8edaca5d41e1a66192036d334e56bd5c7abd
Ovsep Avakian <ovsep.avakian@sumup.com>
2025-08-04 18:09:09 +0300

    [WIP] start working on tcm

--- can_bus/advanced/ecu/transmission.go
+++ can_bus/advanced/ecu/transmission.go
@@ -12,7 +12,6 @@ const (
 var (
 	P0710 = microcontroller.DTC{0x07, 0x10} // Fluid Temperature Sensor Circuit High
 	P0705 = microcontroller.DTC{0x07, 0x05} // Range Sensor Circuit Malfunction
-	P0740 = microcontroller.DTC{0x07, 0x40} // Control System Malfunction
 )
 
 func NewTCM() *can.Node {
