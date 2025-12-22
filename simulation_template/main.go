9f67d2829c85a59d7e8cf314ce482fe8cf42dd27
Ovsep Avakian <ovsep.avakian@sumup.com>
2025-12-22 20:07:35 +0200

    Sim template: add graph generation

--- simulation_template/main.go
+++ simulation_template/main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/hovsep/fmesh-examples/internal"
+)
+
 // This example shows how to turn your fmesh into a simple Discrete Event Simulation (DES) program
 // probably this project be moved to "fmesh-sim" repo in the future
 func main() {
-	NewApp(GetMesh()).Run()
+
+	fm := GetMesh()
+	// Generate graphs if needed
+	err := internal.HandleGraphFlag(fm)
+	if err != nil {
+		fmt.Println("Failed to generate graph: ", err)
+		os.Exit(1)
+	}
+
+	NewApp(fm).Run()
 }
