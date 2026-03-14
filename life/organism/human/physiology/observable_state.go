9d865061a6b17bf9986317aa44fa219d05a99d16
Ovsep Avakian <ovsep.avakian@sumup.com>
2026-03-14 18:00:18 +0200

    fix observable state

--- life/organism/human/physiology/observable_state.go
+++ life/organism/human/physiology/observable_state.go
@@ -4,11 +4,11 @@ import (
 	"github.com/hovsep/fmesh-examples/life/common"
 	"github.com/hovsep/fmesh-examples/life/helper"
 	"github.com/hovsep/fmesh/component"
-	"github.com/hovsep/fmesh/signal"
 )
 
 const (
 	LastBrainActivity                   common.State = "last_brain_activity"
+	IsAlive                             common.State = "is_alive"
 	defaultBrainActivitySmoothingFactor              = 0.1    // alpha in ema
 	defaultBrainActivityThreshold                    = 0.0001 // epsilon in ema
 )
@@ -19,26 +19,17 @@ func GetObservableState() *component.Component {
 		WithDescription("Observable state of the human being (e.g., temperature, blood pressure etc)").
 		WithInitialState(func(st component.State) {
 			st.Set(LastBrainActivity, 0.0)
+			st.Set(IsAlive, false)
 		}).
 		AddInputs("time", "brain_activity", "heart_cardiac_activation", "heart_rate").
 		AddOutputs("brain_activity", "brain_activity_trend", "is_alive", "heart_cardiac_activation", "heart_rate").
 		WithActivationFunc(func(this *component.Component) error {
-			// Are we alive?
-			this.OutputByName("is_alive").PutPayloads(this.InputByName("brain_activity").HasSignals())
 
-			// Calculate brain activity trend
-			currentBrainActivity := helper.AsF64(this.InputByName("brain_activity").Signals().First())
+			brainErr := handleBrainSignals(this)
 
-			lastSmoothedBrainActivity := this.State().Get(LastBrainActivity).(float64)
-			ema := helper.NewEMA(defaultBrainActivitySmoothingFactor, lastSmoothedBrainActivity, defaultBrainActivityThreshold)
-
-			smoothedBrainActivity := ema.Update(currentBrainActivity)
-			brainActivityTrend := ema.ClassifyTrend(currentBrainActivity)
-
-			this.State().Set(LastBrainActivity, smoothedBrainActivity)
-			this.Logger().Printf("Brain activity: %f, trend: %s", smoothedBrainActivity, brainActivityTrend)
-			this.OutputByName("brain_activity").PutSignals(signal.New(smoothedBrainActivity))
-			this.OutputByName("brain_activity_trend").PutSignals(signal.New(brainActivityTrend))
+			if brainErr != nil {
+				return brainErr
+			}
 
 			// Heart signals
 			helper.MultiForward(
@@ -53,4 +44,29 @@ func GetObservableState() *component.Component {
 
 			return nil
 		})
+}
+
+func handleBrainSignals(this *component.Component) error {
+	if !this.InputByName("brain_activity").HasSignals() {
+		// If we don't have brain activity signals, we are dead
+		this.State().Set(IsAlive, false)
+		return nil
+	}
+
+	// We are alive
+	this.State().Set(IsAlive, true)
+
+	// Calculate brain activity trend
+	currentBrainActivity := helper.AsF64(this.InputByName("brain_activity").Signals().First())
+	lastSmoothedBrainActivity := this.State().Get(LastBrainActivity).(float64)
+
+	// Exponential Moving Average helps to determine trend without storing historical data
+	ema := helper.NewEMA(defaultBrainActivitySmoothingFactor, lastSmoothedBrainActivity, defaultBrainActivityThreshold)
+	smoothedBrainActivity := ema.Update(currentBrainActivity)
+	brainActivityTrend := ema.ClassifyTrend(currentBrainActivity)
+
+	this.State().Set(LastBrainActivity, smoothedBrainActivity)
+	this.OutputByName("brain_activity").PutPayloads(smoothedBrainActivity)
+	this.OutputByName("brain_activity_trend").PutPayloads(brainActivityTrend)
+	return nil
 }
