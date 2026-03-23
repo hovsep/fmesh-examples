c2ce7bf3ae2148af3dd5c0deb855de69c4b17506
Ovsep Avakian <ovsep.avakian@sumup.com>
2026-03-23 04:25:30 +0200

    life: add lung component

---
+++ life/organism/human/organ/lung.go
@@ -0,0 +1,43 @@
+package organ
+
+import (
+	"github.com/hovsep/fmesh-examples/life/common"
+	"github.com/hovsep/fmesh/component"
+)
+
+// GetLung returns single lung organ component
+func GetLung(side common.Side) *component.Component {
+	return component.New("organ:lung_"+side).
+		WithDescription("A lung").
+		WithInitialState(func(state component.State) {
+			state.Set("residual_volume", 1200.00)
+			state.Set("volume", 500.00) // Tidal volume
+			state.Set("inspiratory_reserve", 1900.00)
+			state.Set("expiratory_reserve", 700.00)
+			state.Set(common.DamageLevel, defaultDamageLevel)
+		}).
+		AddInputs(
+			"time", //?
+			"autonomic_tone",
+			"diaphragm_contraction",
+			"inspired_gas",
+		).
+		// @TODO: add "intercostal_muscles_contraction", and make it as 20% of lung moving contribution along with the diaphragm (but they must trigger only at exercise time, not resting)
+		AddOutputs(
+			"exhaled_gas",
+			"phase",
+			"volume",
+			"alveolar_pressure",
+			"pleural_pressure",
+			"gas_composition",
+			"respiratory_rate",
+			"inspiration_duration",
+			"exhalation_duration",
+			"lung_efficiency",     // Fraction of oxygen extracted per breath
+			"alveolar_dead_space", //Volume that doesn’t participate in gas exchange
+			"stretch_ratio",       //Current volume / maximum lung volume
+		).
+		WithActivationFunc(func(this *component.Component) error {
+			return nil
+		})
+}
