24ff0cba599ee5e3a75ea498f5722def5f870692
Ovsep Avakian <ovsep.avakian@sumup.com>
2026-05-06 00:39:59 +0300

    Move level helper to signal

--- life/helper/signal.go
+++ life/helper/signal.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"github.com/hovsep/fmesh-examples/life/common"
 	"github.com/hovsep/fmesh/signal"
 )
 
@@ -43,4 +44,14 @@ func AsTypeOrDefault[T any](s *signal.Signal, defaultValue T) T {
 	}
 
 	return s.PayloadOrDefault(defaultValue).(T)
+}
+
+// IsLevel checks if a signal represents a level
+func IsLevel(s *signal.Signal) bool {
+	return s.Labels().ValueIs(common.Type, common.Level)
+}
+
+// NewLevel builds a signal that represents a level
+func NewLevel(value float64, axis string) *signal.Signal {
+	return signal.New(value).AddLabel(common.Type, common.Level).AddLabel(common.Axis, axis)
 }
