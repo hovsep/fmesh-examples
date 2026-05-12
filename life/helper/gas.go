86c946c4fbc209fee5eac25ca58e06a85eaace61
Ovsep Avakian <ovsep.avakian@sumup.com>
2026-05-13 00:48:29 +0300

    [WIP]gas mapping tooling

--- life/helper/gas.go
+++ life/helper/gas.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"github.com/hovsep/fmesh-examples/life/common"
 	"github.com/hovsep/fmesh-examples/life/unit"
 	"github.com/hovsep/fmesh/signal"
 )
@@ -14,34 +15,57 @@ func PackAir(nitrogen, oxygen, argon, pollution, temperature, humidity float64) 
 		signal.NewGroup().Add(
 			NewLevel(temperature*unit.Celsius, "temperature"),
 			NewLevel(humidity*unit.Percent, "humidity"),
-			NewLevel(nitrogen*unit.Percent, "nitrogen").AddLabel("formula", "N2"),
-			NewLevel(oxygen*unit.Percent, "oxygen").AddLabel("formula", "O2"),
-			NewLevel(argon*unit.Percent, "argon").AddLabel("formula", "Ar"),
-			NewLevel(pollution*unit.Percent, "pollution").AddLabel("formula", "PM25"),
+			NewDistribution(DistributionMap{
+				"nitrogen":  nitrogen * unit.Percent,
+				"oxygen":    oxygen * unit.Percent,
+				"argon":     argon * unit.Percent,
+				"pollution": pollution * unit.Percent,
+			}).AddLabel(common.Param, "composition"),
 		)).
 		AddLabel("category", "gas").
 		AddLabel("type", "air")
 }
 
-// MapAirLevel allows modifying a given air level
+// MapAirLevel allows modifying a given air param level (temperature or humidity)
 func MapAirLevel(airSignal *signal.Signal, axis string, mapFunc func(old float64) float64) *signal.Signal {
 	if !IsAir(airSignal) {
 		panic("Signal is not air")
 	}
 
-	return airSignal.MapPayload(func(payload any) any {
-		return payload.(*signal.Group).
-			Map(func(levelSignal *signal.Signal) *signal.Signal {
-				if !IsLevelWithAxis(levelSignal, axis) {
-					// No change
-					return levelSignal
-				}
+	AsGroup(airSignal).
+		Filter(func(s *signal.Signal) bool {
+			return IsLevelWithAxis(s, axis)
+		}).
+		First().
+		MapPayload(func(payload any) any {
+			return mapFunc(payload.(float64))
+		})
 
-				return levelSignal.MapPayload(func(payload any) any {
-					return mapFunc(payload.(float64))
-				})
-			})
-	})
+	return airSignal
+}
+
+// MapAirComposition allows modifying a given air component (nitrogen, oxygen, argon, pollution)
+func MapAirComposition(airSignal *signal.Signal, axis string, mapFunc func(old float64) float64) *signal.Signal {
+	if !IsAir(airSignal) {
+		panic("Signal is not air")
+	}
+
+	compositionSig := AsGroup(airSignal).
+		Filter(func(s *signal.Signal) bool {
+			return s.Labels().ValueIs(common.Param, "composition")
+		}).
+		First()
+
+	AsGroup(compositionSig).
+		Filter(func(s *signal.Signal) bool {
+			return IsLevelWithAxis(s, axis)
+		}).
+		First().
+		MapPayload(func(payload any) any {
+			return mapFunc(payload.(float64))
+		})
+
+	return airSignal
 }
 
 func IsAir(signal *signal.Signal) bool {
